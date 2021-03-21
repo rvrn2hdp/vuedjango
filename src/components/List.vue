eba02f614172cb28d30d9c7a518051aaa025b6f9
rvrn2hdp <rvrn2hdp@gmail.com>
2021-03-21 18:09:55 -0300

    Consumir fetch desde Vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,33 +1,36 @@
 <template>
+
   <div>
-    <div class="box" v-for="e in elements" v-bind:key="e">
-      {{ e }}
+    <div class="box" v-for="e in elements" v-bind:key="e.id">
+      {{ e.title }}
     </div>
 
     <button v-on:click="findAll">Click</button>
-    {{ msj }}
   </div>
+
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      msj: "... :(",
-      elements: ["Uno", "Dos", "Tres"]
+      elements: [] // se define la propiedad o variable que vamos a utilizar
     };
   },
   methods: {
     findAll: function(){
       fetch('http://127.0.0.1:8000/api/element/?format=json') // se hace la peticion a la REST Api
       .then(res => res.json()) // si se recibe algo, se guarda en un json
-      .then(res => console.log(res)) // se imprime en consola
+      .then(res => this.elements = res) // se imprime en consola
     }
   },
 };
+
 </script>
 
 <style>
+
   .box {
     border: 3px solid  #ccc;
     margin: 5px 0 0 0;
