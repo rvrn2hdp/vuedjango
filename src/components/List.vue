d6d21ba4ff5749a741840aaa00b9136825129fd0
rvrn2hdp <rvrn2hdp@gmail.com>
2021-03-21 16:25:39 -0300

    ejemplo de peticion a REST Api

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,6 +3,9 @@
     <div class="box" v-for="e in elements" v-bind:key="e">
       {{ e }}
     </div>
+
+    <button v-on:click="findAll">Click</button>
+    {{ msj }}
   </div>
 </template>
 
@@ -10,8 +13,16 @@
 export default {
   data() {
     return {
-      elements: ["Uno", "Dos", "Tres"],
+      msj: "... :(",
+      elements: ["Uno", "Dos", "Tres"]
     };
+  },
+  methods: {
+    findAll: function(){
+      fetch('http://127.0.0.1:8000/api/element/?format=json') // se hace la peticion a la REST Api
+      .then(res => res.json()) // si se recibe algo, se guarda en un json
+      .then(res => console.log(res)) // se imprime en consola
+    }
   },
 };
 </script>
