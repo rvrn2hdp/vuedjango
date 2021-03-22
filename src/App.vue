536582c8437329143dd000e29f5800ab13e466f8
rvrn2hdp <rvrn2hdp@gmail.com>
2021-03-22 15:43:55 -0300

    componente header añadido

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <!-- img alt="Vue logo" src="./assets/logo.png" -->
-    <List></List>
-    <Detail></Detail>
+    <Header/>
+    <List/>
+    <!--Detail></Detail-->
   </div>
 </template>
 
 <script>
+
+import Header from './components/Header.vue'
 //import HelloWorld from './components/HelloWorld.vue'
 import List from './components/List.vue'
 //import Detail from './components/Detail.vue'
@@ -14,19 +17,9 @@ import List from './components/List.vue'
 export default {
   name: 'App',
   components: {
+    Header,
     List,
     //Detail,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
